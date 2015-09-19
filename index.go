c11a3854e92a6a07e3b15c99d0974db9788fed68
Ivan Pushkin <imetalguardi@gmail.com>
2015-09-19 11:29:49 +0300

    add get job status

--- index.go
+++ index.go
@@ -11,11 +11,39 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"path"
 )
 
 var db, dbError = sql.Open("sqlite3", "./database.db")
+
 const statusProcessing = 0
 const statusDone = 1
+const statusErrorLoad = 2
+const statusErrorParse = 3
+
+const imageStatusProcessing = 0
+const imageStatusDone = 1
+const imageStatusErrorLink = 2
+const imageStatusErrorLoad = 3
+const imageStatusErrorSave = 4
+
+type Error struct {
+	Msg  string
+	Err error
+}
+func (e *Error) Error() string { return e.Msg + ": " + e.Err.Error() }
+
+type Job struct {
+	Id string `json:"id"`
+	Url string `json:"url"`
+	Status string `json:"status"`
+	Images []JobImage `json:"images"`
+}
+
+type JobImage struct {
+	Id string
+	Query string
+}
 
 func main() {
 
@@ -25,11 +53,60 @@ func main() {
 
 	defer db.Close()
 
-	/*r := gin.Default()
+	r := gin.Default()
 	r.GET("/", index)
 	r.GET("/parse", parse)
-	r.Run(":8080")*/
-	parse()
+	r.GET("/parse/:id", getJob)
+	r.Run(":8080")
+}
+
+func getJob(c *gin.Context) {
+	id := c.Param("id")
+	json, err := selectJob(id)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "error", "error": err})
+		return
+	}
+
+	c.JSON(200, json)
+}
+
+func selectJob(id string) (data *Job, err error) {
+	data = &Job{Id: id, Url: "testing"}
+
+	rows, err := db.Query("SELECT * FROM url WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var uid int
+		var url string
+		var status int
+		err = rows.Scan(&status, &uid, &url)
+		if err != nil {
+			return nil, err
+		}
+		data.Url = url
+		data.Status = linkStatus(status)
+	}
+
+	return data, nil
+}
+
+func linkStatus(status int) string {
+	switch status {
+	case statusProcessing:
+		return "processing"
+	case statusErrorParse:
+		return "error parsing html"
+	case statusErrorLoad:
+		return "error loading link"
+	case statusDone:
+		return "done"
+	}
+
+	return "Unknown status"
 }
 
 func checkError(err error) {
@@ -42,23 +119,22 @@ func index(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "Hey"})
 }
 
-func parse() {
-//func parse(c *gin.Context) {
+func parse(c *gin.Context) {
 	link := "http://fie.org/fie/structure/manufacturers"
-	link = "http://www.zaryachem.com/ru"
-
-	// insert
+	//link = "http://www.zaryachem.com/ru"
+	//link = "http://vintage.com.ua"
 
 	u, err := url.Parse(link)
-	checkError(err)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Broken link", "link": link})
+		return
+	}
 
 	id := insertUrl(u.String())
 
-	grab(u, id)
+	go grab(u, id)
 
-	fmt.Printf("\n%v\n\n", id)
-
-	//c.JSON(200, gin.H{"message": "processing", "jobId": id})
+	c.JSON(200, gin.H{"message": "processing", "jobId": id})
 }
 
 func insertUrl(url string) int64 {
@@ -73,7 +149,7 @@ func insertUrl(url string) int64 {
 }
 
 func insertImageUrl(url string, urlId int64) int64 {
-	stmt, err := db.Prepare("INSERT INTO image(url, url_id) values(?, ?)")
+	stmt, err := db.Prepare("INSERT INTO image(link, url_id) values(?, ?)")
 	checkError(err)
 	res, err := stmt.Exec(url, urlId)
 	checkError(err)
@@ -83,14 +159,26 @@ func insertImageUrl(url string, urlId int64) int64 {
 	return id
 }
 
-func grab(u *url.URL, id int64) {
+func grab(u *url.URL, id int64) error {
 	resp, err := http.Get(u.String())
-	checkError(err)
+	if err != nil {
+		linkErrorLoad(id)
+		return err
+	}
 
 	doc, err := html.Parse(resp.Body)
-	checkError(err)
+	if err != nil {
+		linkErrorParse(id)
+		return err
+	}
 
 	getImages(doc, u, id)
+
+	complete(id)
+
+	fmt.Println(id)
+
+	return nil
 }
 
 func getImages(n *html.Node, u *url.URL, id int64) {
@@ -106,36 +194,110 @@ func getImages(n *html.Node, u *url.URL, id int64) {
 	}
 }
 
-func downloadImage(image string, u *url.URL, id int64) {
+func complete(id int64) {
+	linkUpdateStatus(id, statusDone)
+}
 
-	imageLink := createImageLink(image, u)
+func linkErrorLoad (id int64) {
+	linkUpdateStatus(id, statusErrorLoad)
+}
 
-	fmt.Println(imageLink)
+func linkErrorParse (id int64) {
+	linkUpdateStatus(id, statusErrorParse)
+}
+
+func linkUpdateStatus(id int64, status int) {
+	stmt, err := db.Prepare("UPDATE url SET status = ? WHERE id = ?")
+	checkError(err)
+	res, err := stmt.Exec(status, id)
+	checkError(err)
+	id, err = res.LastInsertId()
+	checkError(err)
+}
+
+func imageStatusLink (id int64) {
+	imageUpdateStatus(id, imageStatusErrorLink)
+}
+
+func imageStatusLoad (id int64) {
+	imageUpdateStatus(id, imageStatusErrorLoad)
+}
+
+func imageStatusSave (id int64) {
+	imageUpdateStatus(id, imageStatusErrorSave)
+}
+
+func imageStatusSetDone (id int64) {
+	imageUpdateStatus(id, imageStatusDone)
+}
+
+func imageUpdateStatus(id int64, status int) {
+	stmt, err := db.Prepare("UPDATE image SET status = ? WHERE id = ?")
+	checkError(err)
+	res, err := stmt.Exec(status, id)
+	checkError(err)
+	id, err = res.LastInsertId()
+	checkError(err)
+}
+
+func imageUpdateUrl(id int64, url string) {
+	stmt, err := db.Prepare("UPDATE image SET url = ? WHERE id = ?")
+	checkError(err)
+	res, err := stmt.Exec(url, id)
+	checkError(err)
+	id, err = res.LastInsertId()
+	checkError(err)
+}
+
+func downloadImage(image string, u *url.URL, id int64) error {
+
+	imageId := insertImageUrl(image, id)
+
+	imageLink, err := createImageLink(image, u)
+	if err != nil {
+		imageStatusLink(imageId)
+		return err
+	}
+	imageUpdateUrl(imageId, imageLink)
 
 	resp, err := http.Get(imageLink)
-	checkError(err)
+	if err != nil {
+		imageStatusLoad(imageId)
+		return err
+	}
 
-	imageId := insertImageUrl(imageLink, id)
+	ext := path.Ext(imageLink)
 
 	imageBody, err := ioutil.ReadAll(resp.Body)
 	checkError(err)
 
-	os.Mkdir("/go/files/" + strconv.FormatInt(id, 10), 0777)
-	checkError(err)
+	os.Mkdir("./files/", 0777)
+	os.Mkdir("./files/" + strconv.FormatInt(id, 10), 0777)
 
-	file, err := os.OpenFile("/go/files/" + strconv.FormatInt(id, 10) + "/" + strconv.FormatInt(imageId, 10) + ".png", os.O_CREATE | os.O_RDWR, 0666)
-	checkError(err)
+	file, err := os.OpenFile("./files/" + strconv.FormatInt(id, 10) + "/" + strconv.FormatInt(imageId, 10) + ext, os.O_CREATE | os.O_RDWR, 0666)
 	defer file.Close()
+	if err != nil {
+		imageStatusSave(imageId)
+		return err
+	}
 
-	file.Write(imageBody)
+	_, err = file.Write(imageBody)
+	if err != nil {
+		imageStatusSave(imageId)
+		return err
+	}
+
+	imageStatusSetDone(imageId)
+
+	return nil
 }
 
-func createImageLink(image string, u *url.URL) string {
+func createImageLink(image string, u *url.URL) (imgUrl string, err error) {
 	imageUrl, err := url.Parse(image)
 	if err == nil {
-		return checkUrl(imageUrl, u)
+		return checkUrl(imageUrl, u), nil
 	}
-	return "error"
+	return "", &Error{"parse", err}
 }
 
 func checkUrl(u *url.URL, base *url.URL) string {
